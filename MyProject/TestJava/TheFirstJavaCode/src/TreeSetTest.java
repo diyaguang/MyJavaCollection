20a1e8d09edd39a3430a8b7eb26e1ab4f6ed8398
diyaguang <diyaguang@163.com>
2018-11-29 11:02:03 +0800

    add test code

---
+++ MyProject/TestJava/TheFirstJavaCode/src/TreeSetTest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class TreeSetTest {
+    public static void main(String[] args) {
+        SortedSet<Item> parts = new TreeSet<>();
+        parts.add(new Item("Toaster", 1234));
+        parts.add(new Item("Widget", 4562));
+        parts.add(new Item("odem", 9912));
+        System.out.println(parts);
+
+        NavigableSet<Item> sortByDescription = new TreeSet<>(Comparator.comparing(Item::getDescription));
+        sortByDescription.addAll(parts);
+        System.out.println(sortByDescription);
+    }
+}
