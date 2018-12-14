dbc9a2d7c565092c10b6f35f4b56cca4a3889544
diyaguang <diyaguang@163.com>
2018-12-14 17:45:10 +0800

    add new code

--- MyProject/TestJava/TheFirstJavaCode/src/BlockingQueueTest.java
+++ MyProject/TestJava/TheFirstJavaCode/src/BlockingQueueTest.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -22,7 +23,62 @@ public class BlockingQueueTest {
             System.out.print("Enter keyword (e.g. volatile): ");
             String keyword = in.nextLine();
 
-            
+            Runnable enumerator=()->{
+                try {
+                    enumerate(new File(directory));
+                    queue.put(DUMMY);
+                }
+                catch (InterruptedException e){
+                    //
+                }
+            };
+
+            new Thread(enumerator).start();
+
+            for(int i=1;i<=SEARCH_THREADS;i++){
+                Runnable search=()->{
+                    try{
+                        boolean done = false;
+                        while(!done){
+                            File file = queue.take();
+                            if(file==DUMMY){
+                                queue.put(file);
+                                done=true;
+                            }
+                            else{
+                                search(file,keyword);
+                            }
+                        }
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    catch (InterruptedException e){
+                        //
+                    }
+                };
+                new Thread(search).start();
+            }
+        }
+    }
+
+    public static void enumerate(File directory) throws InterruptedException{
+        File[] files = directory.listFiles();
+        for(File file:files){
+            if(file.isDirectory()) enumerate(file);
+            else queue.put(file);
+        }
+    }
+
+    public static void search(File file,String keyword) throws IOException{
+        try(Scanner in=new Scanner(file,"UTF-8")){
+            int lineNumber=0;
+            while(in.hasNextLine()){
+                lineNumber++;
+                String line=in.nextLine();
+                if(line.contains(keyword))
+                    System.out.printf("%s:%d:%s%n",file.getPath(),lineNumber,line);
+            }
         }
     }
 }
