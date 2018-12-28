92c052379fb001b7d6da1180afd7e2af03d39f47
diyaguang <diyaguang@163.com>
2018-12-28 16:36:07 +0800

    add new code

---
+++ MyProject/TestJava/TheFirstJavaCode/src/BlockingQueueTest2.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * @package: PACKAGE_NAME
+ * @author: diyaguang
+ * @date: 18-12-28 - 上午10:44
+ * @description:
+ */
+public class BlockingQueueTest2 {
+}
---
+++ MyProject/TestJava/TheFirstJavaCode/src/SynchronousQueueTest.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * @package: PACKAGE_NAME
+ * @author: diyaguang
+ * @date: 18-12-28 - 下午2:59
+ * @description:
+ */
+public class SynchronousQueueTest {
+}
