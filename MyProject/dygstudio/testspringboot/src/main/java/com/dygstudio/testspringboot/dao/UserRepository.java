109407f6484d0982f742fc8e4bc724d83197add5
diyaguang <diyaguang@163.com>
2019-02-22 17:51:49 +0800

    update

--- MyProject/dygstudio/testspringboot/src/main/java/com/dygstudio/testspringboot/dao/UserRepository.java
+++ MyProject/dygstudio/testspringboot/src/main/java/com/dygstudio/testspringboot/dao/UserRepository.java
@@ -1,8 +1,14 @@
 package com.dygstudio.testspringboot.dao;
 
 import com.dygstudio.testspringboot.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @Project: dygstudio
@@ -13,4 +19,20 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
     //
+    @Query("select u from User u where u.name=?1 and u.department.id=?2")
+    public User findUser(String name,String departmentId);
+
+    @Query(value = "select * from user where name=?1 and department_id=?2",nativeQuery = true)
+    public User nativeQuery(String name,String departmentId);
+
+    @Query(value = "select department_id,count(*) from user group by depatment_id",nativeQuery = true)
+    public List<Object[]> queryUserCount();
+
+    @Query(value = "select u from User u where u.department.id=?1")
+    public Page<User> queryUsers(String departmentId, Pageable page);
+
+    @Modifying
+    @Query("update User u set u.name=?1  where u.id=?2")
+    public int updateName(String name,String id);
+
 }
--- MyProject/dygstudio/testspringboot/src/main/java/com/dygstudio/testspringboot/service/UserServiceImpl.java
+++ MyProject/dygstudio/testspringboot/src/main/java/com/dygstudio/testspringboot/service/UserServiceImpl.java
@@ -3,14 +3,23 @@ package com.dygstudio.testspringboot.service;
 import com.dygstudio.testspringboot.dao.UserDao;
 import com.dygstudio.testspringboot.dao.UserDaoImpl;
 import com.dygstudio.testspringboot.dao.UserRepository;
+import com.dygstudio.testspringboot.entity.Department;
 import com.dygstudio.testspringboot.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: diyaguang
@@ -28,6 +37,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    EntityManager em;
 
     public List<User> getUserByName(String name){
         User query = new User();
@@ -48,5 +60,54 @@ public class UserServiceImpl implements UserService {
         int totalPage = pageObject.getTotalPages();
         long count = pageObject.getTotalElements();
         return pageObject.getContent();
+    }
+
+    public List<User> getByExample(String name){
+        User user = new User();
+        Department dept = new Department();
+        user.setName(name);
+        dept.setId(1);
+        user.setDepartment(dept);
+        Example<User> example = Example.of(user);
+        List<User> list = userRepository.findAll(example);
+        return list;
+    }
+
+    public Page<User> queryUser2(String departmentId, Pageable page){
+        StringBuilder baseJpql = new StringBuilder("from User u where 1=1");
+        Map<String,Object> paras = new HashMap<String,Object>();
+        if(departmentId!=null){
+            baseJpql.append("and u.department.id=:deptId");
+            paras.put("deptId",departmentId);
+        }
+        long count = getQueryCount(baseJpql,paras);
+        if(count == 0){
+            return new PageImpl<>(Collections.emptyList(),page,0);
+        }
+        List list = getQueryResult(baseJpql,paras,page);
+        Page ret = new PageImpl(list,page,count);
+        return ret;
+    }
+
+    private Long getQueryCount(StringBuilder baseJpql,Map<String,Object> paras){
+        Query query = em.createQuery("select count(1)"+baseJpql.toString());
+        setQueryParameter(query,paras);
+        Number number = (Number)query.getSingleResult();
+        return number.longValue();
+    }
+
+    private void setQueryParameter(Query query,Map<String,Object> paras){
+        for(Map.Entry<String,Object> entry:paras.entrySet()){
+            query.setParameter(entry.getKey(),entry.getValue());
+        }
+    }
+
+    private List getQueryResult(StringBuilder baseJpql,Map<String,Object> paras,Pageable page){
+        Query query = em.createQuery("select u "+baseJpql.toString());
+        setQueryParameter(query,paras);
+        query.setFirstResult((page.getPageNumber()-1)*page.getPageSize());
+        query.setMaxResults(page.getPageNumber());
+        List list = query.getResultList();
+        return list;
     }
 }
